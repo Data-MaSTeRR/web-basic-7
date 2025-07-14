e161f92d9914d8ff308bb3af58bcf615da4788b3
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-07-14 14:42:38 +0900

    Feat: /POST recommend 구현

--- 3-static-dynamic-files-with-template/app.js
+++ 3-static-dynamic-files-with-template/app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 
 const express = require('express')
@@ -5,6 +6,10 @@ const app = express();
 
 // 미들웨어 | public 폴더의 정적파일(html, css, js, 이미지 등) 서빙
 app.use(express.static('public'));
+
+// POST 요청의 폼 데이터를 읽어오기 위한 내장 미들웨어 설정
+// .urlencoded -> body parser 설정 메서드 (to JS코드 변환)
+app.use(express.urlencoded({extended: true}));
 
 // 메인 페이지
 app.get('/', function (req, res) {
@@ -22,6 +27,26 @@ app.get('/restaurants', function (req, res) {
 app.get('/recommend', function (req, res) {
     const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
     res.sendFile(htmlFilePath);
+})
+
+// 추천 restaurants input 전달
+app.post('/recommend', function (req, res) {
+    // requestBody 저장
+    const restaurant = req.body;
+
+    // filePath에서 fileData json.parse 통해 읽어오기
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
+    const fileData = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+
+    // requestBody값 fileData에 저장
+    storedRestaurants.push(restaurant);
+
+    // fileData에 JS 배열 plaintext화 해서 다시 저장
+    fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+    // POST요청 후 confirm.html로 redirect
+    res.redirect('/confirm');
 })
 
 // 확인
