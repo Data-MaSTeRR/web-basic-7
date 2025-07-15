c1428e4fd04517c7ac5646b8d66775c43941eb91
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-07-16 00:39:09 +0900

    Feat: ``(백틱)을 이용하여 동적값 많은 문자열 효율 높이기 | python의 f-string과 유사

--- 5-javascript-advanced/function.js
+++ 5-javascript-advanced/function.js
@@ -1,6 +1,6 @@
 // 필수인자: greetingPrefix, 선택인자: userName
 function greetUser(greetingPrefix, userName = 'User') {
-    console.log(greetingPrefix + " " + userName);
+    console.log(`${greetingPrefix} ${userName}!`);
 }
 
 greetUser('Hi','Eric');
