31d930b42e95dae09a70920ce2392b15a2c58fa4
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-07-17 14:36:56 +0900

    Feat: 객체 블루프린트 class 만들기 - method

--- 5-javascript-advanced/object.js
+++ 5-javascript-advanced/object.js
@@ -1,11 +1,19 @@
+// class
 class Job {
+    // property
     constructor(jobTitle, place, salary){
         this.title = jobTitle;
         this.place = place;
         this.salary = salary;
+    }
+
+    // method ...
+    describe() {
+        console.log(`I'm a ${this.title}, I work in ${this.place}, my salary is ${this.salary}`);
     }
 }
 
 const developer = new Job('Developer', 'New York', 50000);
 const cook = new Job('Cook', 'Tokyo', 30000);
 console.log(cook);
+console.log(cook.describe());
