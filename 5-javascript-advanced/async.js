882ceec9c488b14bcf5772f686bdc99ff7a9ff6d
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-07-18 00:28:13 +0900

    Feat: async/await 이용하여 비동기처리 단순화 + try/catch 가능 | 동시에 여러 비동기 작업 실행가능

--- 5-javascript-advanced/async.js
+++ 5-javascript-advanced/async.js
@@ -18,6 +18,7 @@ function readFile() {
 readFile();
  */
 
+/*
 const fs = require('fs/promises');
 
 function readFile() {
@@ -32,6 +33,25 @@ function readFile() {
         });
 
     // 위 읽기 작업이 비동기라, 'Hello'가 제일 먼저 실행됨
+    console.log('Hello')
+}
+
+readFile();
+ */
+
+const fs = require('fs/promises');
+
+// async/wait를 통해 promise-chain 단순화
+async function readFile() { // async -> 자동으로 promise 반환
+    let fileData;
+    try { // await는 promise 해결까지 일시적으로 함수 실행을 중단(코드 계속 아래로)
+        fileData = await fs.readFile('data.txt')
+    } catch(err) {
+        console.log(err.message);
+    }
+
+    console.log('File parsing done!');
+    console.log(fileData.toString());
     console.log('Hello')
 }
 
