0b3f21bf76aee67d3cfe79eeec216cbdc75e0ba6
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-07-17 15:43:12 +0900

    Feat: 비동기처리 중 error 처리

--- 5-javascript-advanced/async.js
+++ 5-javascript-advanced/async.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 function readFile() {
     // 2번째 arg -> txt 구문 분석 후 실행되어야 할 익명함수(콜백함수) | 비동기
     fs.readFile('data.txt', function(err, fileData) {
+        if (err) {
+            console.log('error!');
+        }
         console.log('File parsing done!');
         console.log(fileData.toString());
     });
@@ -23,8 +26,10 @@ function readFile() {
         .then(function(fileData) {
             console.log('File parsing done!');
             console.log(fileData.toString());
+        })
+        .catch(function(err) {
+            console.log(err);
         });
-
 
     // 위 읽기 작업이 비동기라, 'Hello'가 제일 먼저 실행됨
     console.log('Hello')
