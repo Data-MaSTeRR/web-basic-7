38265a269996a3cd915cb855399340a1eeb40cd3
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-07-17 14:37:53 +0900

    Feat: 객체 비구조화

--- 5-javascript-advanced/object.js
+++ 5-javascript-advanced/object.js
@@ -17,3 +17,7 @@ const developer = new Job('Developer', 'New York', 50000);
 const cook = new Job('Cook', 'Tokyo', 30000);
 console.log(cook);
 console.log(cook.describe());
+
+// 객체 비구조화 -> 객체에서 속성값 상수로 뽑기
+const {v4: uuidv4} = require('uuid');
+console.log(uuidv4());
