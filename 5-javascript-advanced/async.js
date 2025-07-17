23687d3af8eece8732bffa909e6a841f7b62e160
Data-MaSTeRR <hyunwoo8504@gmail.com>
2025-07-17 15:42:47 +0900

    Feat: promise를 이용한 직관적인 비동기처리 (promise-chain)

--- 5-javascript-advanced/async.js
+++ 5-javascript-advanced/async.js
@@ -1,3 +1,4 @@
+/*
 const fs = require('fs');
 
 function readFile() {
@@ -6,6 +7,24 @@ function readFile() {
         console.log('File parsing done!');
         console.log(fileData.toString());
     });
+
+    // 위 읽기 작업이 비동기라, 'Hello'가 제일 먼저 실행됨
+    console.log('Hello')
+}
+
+readFile();
+ */
+
+const fs = require('fs/promises');
+
+function readFile() {
+    // 2번째 arg -> txt 구문 분석 후 실행되어야 할 익명함수(콜백함수) | 비동기
+    fs.readFile('data.txt')
+        .then(function(fileData) {
+            console.log('File parsing done!');
+            console.log(fileData.toString());
+        });
+
 
     // 위 읽기 작업이 비동기라, 'Hello'가 제일 먼저 실행됨
     console.log('Hello')
